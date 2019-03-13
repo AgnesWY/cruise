e7046b2de5f51e1faa21e4240a5d7c5e214a9358
Wangyue <wangyue@danlu.com>
2019-03-13 09:36:18 +0800

    添加element-ui

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -65,13 +65,7 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
-      { test: /\.css$/,
-        use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-        }, {
-          loader: "css-loader" // translates CSS into CommonJS
-        }]
-      },
+      
       { test: /\.less$/,
         use: [{
           loader: "style-loader" // creates style nodes from JS strings
